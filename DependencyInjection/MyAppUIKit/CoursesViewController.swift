608a3f843fe8cbfda87718fb57c9aa6aeeb837b9
VeronikaPadalikova <xpadali3@mendelu.cz>
2022-01-17 20:19:08 +0100

    Dependency injection

---
+++ DependencyInjection/MyAppUIKit/CoursesViewController.swift
@@ -0,0 +1,79 @@
+//
+//  CoursesViewController.swift
+//  MyAppUIKit
+//
+//  Created by Veronika Padalíková on 17.01.2022.
+//
+
+import UIKit
+
+public protocol DataFetchable {
+    func fetchCourseNames(completion: @escaping ([String]) -> Void)
+}
+
+struct Course {
+    let name: String
+}
+
+public class CoursesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let dataFetchable: DataFetchable
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    public init(dataFetchable: DataFetchable){
+        self.dataFetchable = dataFetchable
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    public required init?(coder: NSCoder) {
+        fatalError()
+    }
+
+    var courses: [Course] = []
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(tableView)
+        tableView.dataSource = self
+        tableView.delegate = self
+        view.backgroundColor = .systemBackground
+        dataFetchable.fetchCourseNames{ [weak self] names in
+            self?.courses = names.map {Course(name: $0)}
+            DispatchQueue.main.async {
+                self?.tableView.reloadData()
+            }
+        }
+    }
+    
+    public override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return courses.count
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = courses[indexPath.row].name
+        return cell
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
